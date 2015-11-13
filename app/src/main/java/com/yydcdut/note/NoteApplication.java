34022c52e6b06c29aac0c53ef84abd22fc7fd23e
yuyidong <378040621@qq.com>
2015-11-13 14:04:52 +0800

    Update

--- app/src/main/java/com/yydcdut/note/NoteApplication.java
+++ app/src/main/java/com/yydcdut/note/NoteApplication.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import com.baidu.mapapi.SDKInitializer;
 import com.evernote.client.android.EvernoteSession;
 import com.iflytek.cloud.SpeechUtility;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 import com.umeng.analytics.MobclickAgent;
 import com.yydcdut.note.model.PhotoNoteDBModel;
 import com.yydcdut.note.model.UserCenter;
@@ -34,7 +36,7 @@ public class NoteApplication extends Application {
     private static NoteApplication mInstance;
     private static final int MAX_THREAD_POOL_NUMBER = 5;
     private ExecutorService mPool;
-//    private RefWatcher mRefWatcher;
+    private RefWatcher mRefWatcher;
 
     private static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.PRODUCTION;
     private static final boolean SUPPORT_APP_LINKED_NOTEBOOKS = true;
@@ -63,7 +65,7 @@ public class NoteApplication extends Application {
         mInstance = NoteApplication.this;
         super.onCreate();
 
-//        mRefWatcher = LeakCanary.install(this);
+        mRefWatcher = LeakCanary.install(this);
 
         initImageLoader();
         initExecutor();
@@ -81,10 +83,10 @@ public class NoteApplication extends Application {
         }
 
         //打点
-        MobclickAgent.setDebugMode(false);
+        MobclickAgent.setDebugMode(true);
         MobclickAgent.openActivityDurationTrack(true);
         MobclickAgent.updateOnlineConfig(this);
-        MobclickAgent.setCatchUncaughtExceptions(true);
+        MobclickAgent.setCatchUncaughtExceptions(false);
 
 //        CrashHandler.getInstance().init(getApplicationContext());
 
