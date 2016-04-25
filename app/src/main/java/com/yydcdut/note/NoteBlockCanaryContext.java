4a5cdb0c077aff45d83acd8c421c32c5671dda3e
yuyidong <378040621@qq.com>
2016-04-25 10:40:29 +0800

    add BlockCanary

--- app/src/main/java/com/yydcdut/note/NoteBlockCanaryContext.java
+++ app/src/main/java/com/yydcdut/note/NoteBlockCanaryContext.java
@@ -1,66 +1,73 @@
 package com.yydcdut.note;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import com.github.moduth.blockcanary.BlockCanaryContext;
+import com.yydcdut.note.utils.YLog;
+
+import java.io.File;
+
 /**
  * Created by yuyidong on 16/1/20.
  */
-public class NoteBlockCanaryContext
-//        extends BlockCanaryContext
-{
-//    private Context mContext;
-//
-//    public NoteBlockCanaryContext(Context context) {
-//        mContext = context;
-//    }
-//
-//    @Override
-//    public String getQualifier() {
-//        String qualifier = "";
-//        try {
-//            PackageInfo info = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
-//            qualifier += info.versionCode + "_" + info.versionName + "_YYB";
-//        } catch (PackageManager.NameNotFoundException e) {
-//            YLog.e("yuyidong", "getQualifier exception  " + e);
-//        }
-//        return qualifier;
-//    }
-//
-//    @Override
-//    public String getUid() {
-//        return "87224330";
-//    }
-//
-//    @Override
-//    public String getNetworkType() {
-//        return "Wifi";
-//    }
-//
-//    @Override
-//    public int getConfigDuration() {
-//        return 9999;
-//    }
-//
-//    @Override
-//    public int getConfigBlockThreshold() {
-//        return 500;
-//    }
-//
-//    @Override
-//    public boolean isNeedDisplay() {
-//        return BuildConfig.DEBUG;
-//    }
-//
-//    @Override
-//    public String getLogPath() {
-//        return "/blockcanary/performance";
-//    }
-//
-//    @Override
-//    public boolean zipLogFile(File[] files, File file) {
-//        return false;
-//    }
-//
-//    @Override
-//    public void uploadLogFile(File file) {
-//
-//    }
+public class NoteBlockCanaryContext extends BlockCanaryContext {
+    private Context mContext;
+
+    public NoteBlockCanaryContext(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public String getQualifier() {
+        String qualifier = "";
+        try {
+            PackageInfo info = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
+            qualifier += info.versionCode + "_" + info.versionName + "_YYB";
+        } catch (PackageManager.NameNotFoundException e) {
+            YLog.e("yuyidong", "getQualifier exception  " + e);
+        }
+        return qualifier;
+    }
+
+    @Override
+    public String getUid() {
+        return "87224330";
+    }
+
+    @Override
+    public String getNetworkType() {
+        return "Wifi";
+    }
+
+    @Override
+    public int getConfigDuration() {
+        return 9999;
+    }
+
+    @Override
+    public int getConfigBlockThreshold() {
+        return 500;
+    }
+
+    @Override
+    public boolean isNeedDisplay() {
+        return BuildConfig.DEBUG;
+    }
+
+    @Override
+    public String getLogPath() {
+        return "/blockcanary/performance";
+    }
+
+    @Override
+    public boolean zipLogFile(File[] files, File file) {
+        return false;
+    }
+
+    @Override
+    public void uploadLogFile(File file) {
+
+    }
 }
