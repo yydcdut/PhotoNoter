21bb28070bc727ec42ac9e2fd939e93d27618b85
yuyidong <378040621@qq.com>
2016-04-29 10:04:35 +0800

    fix

--- app/src/main/java/com/yydcdut/note/widget/CircleProgressBar.java
+++ app/src/main/java/com/yydcdut/note/widget/CircleProgressBar.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.yydcdut.note.widget;
 
 import android.content.Context;
@@ -35,11 +19,6 @@ import android.widget.ImageView;
 
 import com.yydcdut.note.R;
 
-/**
- * Private class created to work around issues with AnimationListeners being
- * called before the animation is actually complete and support shadows on older
- * platforms.
- */
 public class CircleProgressBar extends ImageView {
 
     private static final int KEY_SHADOW_COLOR = 0x1E000000;
