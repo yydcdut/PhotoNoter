165cf61cd9d66e6842cddea457a1b299d83f2e93
yuyidong <378040621@qq.com>
2016-05-03 13:17:16 +0800

    add AndroidDevMetrics

--- app/src/main/java/com/yydcdut/note/NoteApplication.java
+++ app/src/main/java/com/yydcdut/note/NoteApplication.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Environment;
 
+import com.frogermcs.androiddevmetrics.AndroidDevMetrics;
 import com.squareup.leakcanary.LeakCanary;
 import com.umeng.analytics.MobclickAgent;
 import com.yydcdut.note.injector.component.ApplicationComponent;
@@ -44,13 +45,14 @@ public class NoteApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        if (isDexProcess()) {
+//        if (isDexProcess()) {a
 //            return;
 //        }
 
         if (BuildConfig.LOG_DEBUG) {
             LeakCanary.install(this);
 //            BlockCanary.install(this, new NoteBlockCanaryContext(this)).start();
+            AndroidDevMetrics.initWith(this);
         }
 
         initComponent();
