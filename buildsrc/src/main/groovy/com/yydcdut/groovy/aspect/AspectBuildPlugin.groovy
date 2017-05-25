0af2ef19a2abf725d7c053d912bef8d24e8185a6
yuyidong <378040621@qq.com>
2017-05-25 17:32:45 +0800

    Add buildSrc

---
+++ buildsrc/src/main/groovy/com/yydcdut/groovy/aspect/AspectBuildPlugin.groovy
@@ -0,0 +1,57 @@
+package com.yydcdut.groovy.aspect
+
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+
+class AspectBuildPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        final def log = project.logger
+        final def variants = project.android.applicationVariants
+
+        variants.all { variant ->
+            if (!variant.buildType.isDebuggable()) {
+                log.debug("Skipping non-debuggable build type '${variant.buildType.name}'.")
+                return
+            }
+
+            JavaCompile javaCompile = variant.javaCompile
+            javaCompile.doLast {
+                String[] args = ["-showWeaveInfo",
+                                 "-1.5",
+                                 "-inpath", javaCompile.destinationDir.toString(),
+                                 "-aspectpath", javaCompile.classpath.asPath,
+                                 "-d", javaCompile.destinationDir.toString(),
+                                 "-classpath", javaCompile.classpath.asPath,
+                                 "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)]
+                log.debug "ajc args: " + Arrays.toString(args)
+
+                MessageHandler handler = new MessageHandler(true)
+                new Main().run(args, handler)
+                for (IMessage message : handler.getMessages(null, true)) {
+                    switch (message.getKind()) {
+                        case IMessage.ABORT:
+                        case IMessage.ERROR:
+                        case IMessage.FAIL:
+                            log.error message.message, message.thrown
+                            break
+                        case IMessage.WARNING:
+                            log.warn message.message, message.thrown
+                            break
+                        case IMessage.INFO:
+                            log.info message.message, message.thrown
+                            break
+                        case IMessage.DEBUG:
+                            log.debug message.message, message.thrown
+                            break
+                    }
+                }
+            }
+        }
+    }
+}
